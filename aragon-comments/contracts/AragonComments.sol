54d6bef19cc45cf4416dad37887ae869450f8351
Maxime Cote <maxime.cote184@gmail.com>
2018-10-19 17:27:31 -0400

    Add Comment struct

--- aragon-comments/contracts/AragonComments.sol
+++ aragon-comments/contracts/AragonComments.sol
@@ -8,19 +8,34 @@ contract AragonComments is AragonApp {
 
     event NewComment(address indexed entity);
 
+    struct Comment {
+        address author;
+        uint256 date;
+        string message;
+    }
+
+    Comment[] public comments;
     string private currentComment;
-    uint private currentValue;
 
     function initialize() onlyInit public {
         initialized();
 
         currentComment = "initial comment";
-        currentValue = 123;
+
+        comments.push(Comment({
+            author: 0,
+            date: 123,
+            message: "Hello!"
+        }));
     }
 
     function postComment(string _comment) public {
         currentComment = _comment;
         emit NewComment(msg.sender);
+    }
+
+    function commentsCount() external view returns (uint256) {
+        return comments.length;
     }
 
     function getComment() external view returns (string) {
